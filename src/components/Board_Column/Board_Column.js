b797936170303630cbf06ad1c0cd6c3d0668a4b1
Hoangduc1203BK <ducgiang190520@gmail.com>
2021-12-21 23:15:12 +0700

    create_new_column

--- src/components/Board_Column/Board_Column.js
+++ src/components/Board_Column/Board_Column.js
@@ -1,13 +1,23 @@
 import "./Board_Column.scss";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import { initialValue } from "../../actions/InitialValue";
 import { Container, Draggable } from "react-smooth-dnd";
 import { isEmpty } from "lodash";
 import { applyDrag } from "../../lib/lib";
 import Column from "../Column/Column";
+import {
+  Container as AddContainer,
+  Row,
+  Col,
+  Form,
+  Button,
+} from "react-bootstrap";
 function Board_Column() {
   const [board, setBoard] = useState({});
   const [columns, setColumns] = useState([]);
+  const [openAdd, setOpenAdd] = useState(false);
+  const [newTileColumns, setNewTileColumns] = useState("");
+  const inputRef = useRef();
   useEffect(() => {
     const boardDB = initialValue.boards.find(
       (boards) => boards.id === "board-1"
@@ -23,6 +33,12 @@ function Board_Column() {
       setColumns(columnDB);
     }
   }, []);
+  useEffect(() => {
+    if (openAdd && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [openAdd]);
+  const handleChangeInput=useCallback((e)=>setNewTileColumns(e.target.value),[])
   if (isEmpty(board)) {
     return (
       <div className="error" style={{ paddingLeft: 10, color: "white" }}>
@@ -49,6 +65,32 @@ function Board_Column() {
       setColumns(newColumns);
     }
   };
+  const openAddColumn = () => {
+    // newColumnRef.current.style.display = 'block'
+    setOpenAdd(!openAdd);
+  };
+  const handleSubmit = () => {
+    if(!newTileColumns){
+      inputRef.current.focus();
+      return
+    }
+    const newColumn={
+      id:Math.random().toString(36).substr(2,5),
+      boardId:board.id,
+      title:newTileColumns.trim(),
+      cardOrder:[],
+      cards:[]
+    }
+    const cloneColumn=[...columns]
+    cloneColumn.push(newColumn)
+    let newBoard = { ...board };
+    newBoard.columnOrder = cloneColumn.map((el) => el.id);
+    newBoard.columns = cloneColumn;
+    setColumns(cloneColumn);
+    setBoard(newBoard);
+    setNewTileColumns('')
+    setOpenAdd(false)
+  };
   return (
     <div className="Board_column">
       <Container
@@ -68,10 +110,39 @@ function Board_Column() {
           </Draggable>
         ))}
       </Container>
-      <div className="Add-new-column">
-        <i className="fa fa-plus icon" />
-        Add another column
-      </div>
+      <AddContainer className="trello-add-column">
+        {!openAdd && (
+          <Row>
+            <Col className="Add-new-column" onClick={openAddColumn}>
+              <i className="fa fa-plus " /> Add another column
+            </Col>
+          </Row>
+        )}
+        {openAdd && (
+          <Row>
+            <Col className="enter-add-new-column">
+              <Form.Control
+                ref={inputRef}
+                className="input-add-new-column"
+                size="sm"
+                type="text"
+                placeholder="Add new column"
+                value={newTileColumns}
+                onChange={handleChangeInput}
+                onKeyDown={e=> e.key==='Enter' && handleSubmit()}
+              />
+              <Button
+                className="enter_button"
+                variant="success"
+                onClick={handleSubmit}
+              >
+                Success
+              </Button>
+              <i onClick={openAddColumn} className="fa fa-times icon" />
+            </Col>
+          </Row>
+        )}
+      </AddContainer>
     </div>
   );
 }
